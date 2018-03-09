bd42f4bfab486ef9e98eb86bd6fecadf2e79e722
BruceSmile <1634372267@qq.com>
2018-03-09 21:38:11 +0800

    Revert "change2"
    This reverts commit adb70aabf71bcde3636552ad188bfaa1cb0a0f05.

--- exam1/src/java/Test.java
+++ exam1/src/java/Test.java
@@ -6,6 +6,5 @@ public class MathCount {
 		System.out.println("直接在浏览器改项目文件内容");
 		System.out.println("这次是在客户端修改项目名和项目文件内容");
 		System.out.println("这是第一次修改，第二次修改后回滚到这一次修改");
-		System.out.println("这是第二次修改，准备回滚到上一次修改");
 	}
 }
