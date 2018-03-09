b8400f1b6581f682ee065d4c5ef9ba19a028aef9
BruceSmile <1634372267@qq.com>
2018-03-09 21:35:27 +0800

    changeProjectNameAndInfo
    直接在浏览器改项目文件内容

--- exam1/src/java/Test.java
+++ exam1/src/java/Test.java
@@ -4,5 +4,6 @@ public class MathCount {
 	public static void main(String[] args) {
 		System.out.println("这是测试项目修改");
 		System.out.println("直接在浏览器改项目文件内容");
+		System.out.println("这次是在客户端修改项目名和项目文件内容");
 	}
 }
