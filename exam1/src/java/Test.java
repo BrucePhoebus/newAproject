41452a1ad4ebfe1066dfa2a191cc4e3b7b3f222b
BruceSmile <1634372267@qq.com>
2018-03-09 21:37:11 +0800

    change
    回滚测试1

--- exam1/src/java/Test.java
+++ exam1/src/java/Test.java
@@ -5,5 +5,6 @@ public class MathCount {
 		System.out.println("这是测试项目修改");
 		System.out.println("直接在浏览器改项目文件内容");
 		System.out.println("这次是在客户端修改项目名和项目文件内容");
+		System.out.println("这是第一次修改，第二次修改后回滚到这一次修改");
 	}
 }
